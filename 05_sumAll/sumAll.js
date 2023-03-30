ed15a0f7df701fe2b8bcd21a7a3ac66d531da306
ceaweed <cwellen.w@gmail.com>
2023-03-30 12:52:36 -0500

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(beginNum, endNum) {
+    let sum = 0;
+    if (typeof beginNum == "number" && typeof endNum == "number" &&
+        beginNum >= 0 && endNum >= 0) {
+        
+        let smallerNum = 0;
+        let largerNum = 0;
+        if (beginNum >= endNum) {
+            largerNum = beginNum;
+            smallerNum = endNum;
+        } else {
+            smallerNum = beginNum;
+            largerNum = endNum;
+        }
 
+        for (let i = smallerNum; i <= largerNum; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
